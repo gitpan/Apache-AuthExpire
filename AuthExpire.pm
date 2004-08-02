9a08ad4193b68430091e01c384b3b543f9bd8ec6
Shannon Eric Peevey <speeves@erikin.com>
2004-08-02 11:24:58 +0000

    Import of SPEEVES/Apache-AuthExpire-0.39 from CPAN.
    gitpan-cpan-distribution: Apache-AuthExpire
    gitpan-cpan-version:      0.39
    gitpan-cpan-path:         SPEEVES/Apache-AuthExpire-0.39.tar.gz
    gitpan-cpan-author:       SPEEVES
    gitpan-cpan-maturity:     released

--- AuthExpire.pm
+++ AuthExpire.pm
@@ -2,22 +2,42 @@ package Apache::AuthExpire;
 #file Apache/AuthExpire.pm
 #
 #	Author: J. J. Horner
-#       Revisions:  Shannon Eric Peevey <speeves@unt.edu>
-#	Version: 0.38 (07/15/2003)
+#       Revisions:  Shannon Eric Peevey <speeves@erikin.com>
+#	Version: 0.39 (07/29/2004)
 #	Usage:  see documentation
 #	Description:
-#		Small mod_perl handler to provide Authentication phase time outs for 
-#		sensitive areas, per realm.  Still has a few issues, but nothing too 
-#		serious.
+#		Small mod_perl handler to provide Authentication phase 
+#               time outs for sensitive areas, per realm.  Still has a 
+#               few issues, but nothing too serious.
+#
 
 use strict;
 use Carp;
 use mod_perl;
+require Exporter;
+our @ISA = qw(Exporter);
 
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration use Apache::AuthExpire ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw() ] );
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+our @EXPORT = qw();
+
+#---------------------------------------------------------------------------
+#VERSION
+our $VERSION = '0.39';
+
+#---------------------------------------------------------------------------
 # setting the constants to help identify which version of mod_perl
 # is installed
 use constant MP2 => ($mod_perl::VERSION >= 1.99);
 
+#---------------------------------------------------------------------------
 # test for the version of mod_perl, and use the appropriate libraries
 BEGIN {
         if (MP2) {
@@ -26,9 +46,11 @@ BEGIN {
                 require Apache::Connection;
                 require Apache::Log;
                 require Apache::RequestRec;
-                require Apache::RequestUtil;
-		require Apache::ServerUtil;
-                Apache::Const->import(-compile => 'HTTP_UNAUTHORIZED','OK','DECLINED');
+                require Apache::RequestUtil; 
+                require Apache::ServerUtil;
+		require APR::Table;
+                Apache::Const->import(-compile => 'HTTP_UNAUTHORIZED',
+                                                  'OK','DECLINED');
         } else {
                 require Apache::Constants;
 		require Apache::Log;
@@ -36,69 +58,118 @@ BEGIN {
         }
 }
 
-our $VERSION = '0.38';
-
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
 sub handler {
 
     my $current_time = time();    # Time will be used here :)
 
     my $r = shift;
     my $log = $r->log;
-    return MP2 ? Apache::DECLINED : Apache::Constants::DECLINED unless $r->is_initial_req;
+    
+    # check to see if this is the initial request, and pass
+    # off to the next Handler, if it is not.
+    return MP2 ? Apache::DECLINED : Apache::Constants::DECLINED 
+        unless($r->is_initial_req);
 
     #grab debug value from config files.
     #Sends 'debug' level messages to error_log when set. 
     my $DEBUG;
 
-    if (defined ($r->dir_config('TIMEOUT_DEBUG'))) { 
-        $DEBUG = $r->dir_config('TIMEOUT_DEBUG'); 
+    if (defined ($r->dir_config('TimeoutDebug'))) { 
+        $DEBUG = $r->dir_config('TimeoutDebug'); 
         $log->notice("Debug value set to $DEBUG.");
     }
-
+    
+    my $default = $r->dir_config('DefaultLimit') || 60 ;
+    my $limit = $r->dir_config('TimeLimit') || $default ;
+    my $timeoutpurge = $r->dir_config('TimeoutPurge') || undef ;
+    my $allowalternateauth = $r->dir_config('AllowAlternateAuth') || "";
+    my $conftimes = $r->dir_config('TimeFileDir') || "logs/authexpire";
+ 
     my ($res, $sent_pw) = $r->get_basic_auth_pw;
-    return $res if $res !=  (MP2 ? Apache::OK : OK);  # return not OK status if not OK
 
-    my ($limit, $default, $time_to_die);
+    # Pass request to next Handler if not using Basic Authentication
+    return $res 
+        if($res !=  ((MP2) ? Apache::OK : Apache::Constants::OK));
+
     my $request_line = $r->the_request;
 
     # Grab TimeLimit from .htaccess file (if available)
     # or use DefaultLimit if TimeLimit not set or if
     # TimeLimit greater than default.  Can't have longer
     # time limits than max set by policy.
-    
-    $limit = $r->dir_config('TimeLimit') if (defined($r->dir_config('TimeLimit')) && $r->dir_config('TimeLimit') > 1);
-    $default = $r->dir_config('DefaultLimit');
+
+    # $DEBUG is set, print this to log file with
+    # Default time limit
     $log->notice("Default Limit set to $default.") if ($DEBUG);
 
-	if (defined($limit)) {
-    	$time_to_die = ($limit < $default) ? $limit : $default;
-		$log->notice("Time Limit for $request_line set to $limit") if ($DEBUG);
-	} else {
-		$time_to_die = $default;
-	}	
+    if ($limit > $default) {
+	$limit = $default;
+	$log->notice("Time Limit for $request_line set to $limit") 
+        	if ($DEBUG);
+	}
 	
-    # Do nothing if MODE set to 'Off'.
-    return MP2 ? Apache::DECLINED : Apache::Constants::DECLINED if ($r->dir_config('MODE') eq 'Off');
-
     my $user = MP2 ? $r->user : $r->connection->user;
     my $realm = $r->auth_name();
     $realm =~ s/\s+/_/g;
     $realm =~ s/\//_/g;
     my $host = MP2 ? $r->connection->get_remote_host() : $r->get_remote_host();
-    my $time_file = MP2 ? Apache::server_root_relative($r->pool, "conf/times/$realm-$host.$user") :$r->server_root_relative("conf/times/$realm-$host.$user");
+
+    # Check for existence of X-Forwarded-For header, which specifies that the
+    # request has been sent through a proxy.  We do this to grab the IP of the
+    # original client, bypassing the problem created by all clients coming from
+    # the same address.
+    my $xforwardedfor = MP2 ? $r->headers_in->{'X-Forwarded-For'} : $r->header_in('X-Forwarded-For');
+    if(defined($xforwardedfor))
+    {
+        $host = $xforwardedfor;
+        $log->notice("Client: $xforwardedfor is being proxied in from $host") if ($DEBUG);
+    }
+
+    #Pre-check delete all timeout files older than N hours according
+    # to TIMEOUT_PURGE value. ---bcw
+    if (defined ($timeoutpurge))
+    {
+        $log->notice("TimeoutPurge value set to $timeoutpurge.") if ($DEBUG);
+    }
+
+    #The conftimes directory.
+    my $time_file = "$conftimes/$realm-$host.$user";
+
+    $time_file = (MP2) ? Apache::server_root_relative($r->pool,$time_file) : 
+                         $r->server_root_relative($time_file);
     $log->notice("Time file set to $time_file") if ($DEBUG);
+
+    #Do the $TimeoutPurge check here. ---bcw
+    if($timeoutpurge && -e $time_file)
+    {
+        my $time = (stat($time_file))[9];
+        if(60 * $timeoutpurge < $current_time - $time)
+        {
+            unlink($time_file);
+            $log->notice("Time file deleted, as it has lived beyond its TimeoutPurge limit: $timeoutpurge.")
+                if ($DEBUG);
+        }
+    }
+
     if (-e $time_file) {   # if timestamp file exists, check time difference
         my $last_time = (stat($time_file))[9] 
             || $log->warn("Unable to get last modtime from file: $!");
 
-        my $time_delta = ($current_time - $last_time); # Determine time since last access
-       if ($time_to_die >  $time_delta) {
-        	# time delta = specified time limit
+        # Determine time since last access
+        my $time_delta = ($current_time - $last_time);
+        if ($limit >  $time_delta) {
+            # time delta = specified time limit
             open (TIME, ">$time_file") 
                 || $log->warn("Can't update timestamp on $time_file: $!");
             close TIME;
-            return MP2 ? Apache::OK : OK;
-
+	    if ($allowalternateauth eq 'yes'){
+		    return MP2 ? Apache::DECLINED : Apache::Constants::DECLINED;
+	    } else {
+		    return MP2 ? Apache::OK : Apache::Constants::OK;
+	    }
+	    
         } else {  # time delta greater than TimeLimit
             $log->notice("Time since last access: $time_delta") if ($DEBUG);
             $r->note_basic_auth_failure;
@@ -109,10 +180,14 @@ sub handler {
     } else {  
     # previous time delta greater than TimeLimit so file was unlinked
     # or first time checking into server.
-        
-        open (TIME, ">$time_file") || $log->crit("Unable to create $time_file: $!\n");
+        open (TIME, ">$time_file") || 
+            $log->crit("Unable to create $time_file: $!\n");
         close TIME;
-        return MP2 ? Apache::OK : OK;
+        if ($allowalternateauth eq 'yes'){
+	    return MP2 ? Apache::DECLINED : Apache::Constants::DECLINED;
+ 	} else {
+	    return MP2 ? Apache::OK : Apache::Constants::OK;
+	}
     }
 }
 
@@ -127,31 +202,71 @@ Apache::AuthExpire - mod_perl handler to provide Authentication time limits on .
 
   In httpd.conf file:
 	PerlAuthenHandler Apache::AuthExpire
-	PerlSetVar DefaultLimit <timeout in seconds>
 
   Optional httpd.conf file entry:
-	PerlSetVar TIMEOUT_DEBUG <0 || 1>
-	   Turns debugging on to print messages to server error_log
-
+	PerlSetVar DefaultLimit <seconds>
+        PerlSetVar TimeFileDir /location/of/timefilesdir
+	PerlSetVar AllowAlternateAuth yes 
+	PerlSetVar TimeoutDebug <0 || 1>
+	
   Optional .htaccess entries: 
-	PerlSetVar TimeLimit <time>
-	    or
-	PerlSetVar MODE Off        # to turn off timeouts
-                                # Will provide further methods later.
+	PerlSetVar TimeLimit <seconds>
+
+  Optional .htaccess or httpd.conf entries: 
+        PerlSetVar TimeoutPurge <minutes> 
 
 =head1 DESCRIPTION
 
-  Simple mod_perl handler for the AUTHENTICATION phase to set a limit on user inactivity.
-  Will provide timeouts to any file under the protection of an .htaccess file, unless the 
-  'MODE' option set to anything other than 0 in the .htaccess file.  The 'DefaultLimit' is
-  set via the httpd.conf file, and unless the user specified 'TimeLimit' is set and less 
-  than the 'DefaultLimit', determines the length of time a user can be inactive.  This 
-  handler can be set anywhere an AUTHENTICATION handler can be specified.
+This is a simple mod_perl handler for the Authentication phase which sets a 
+time limit based on user inactivity. It provides timeouts for any file under 
+the protection of a .htaccess file. This handler can be set anywhere an 
+Authentication handler can be specified.
+
+=head1 CONFIGURATION
+
+=head2 PerlSetVar DefaultLimit <seconds>
+
+Set the Default timeout limit for our session.  (time in seconds)  This is also used as the
+'TimeLimit', if the 'TimeLimit' is not explicitly defined. Defaults to 60 seconds.  
+
+=head2 PerlSetVar TimeFileDir /path
+
+Set the timeout file directory to an alternate location. 
+Default is <SERVERROOT>/logs/authexpire.
+
+=head2 PerlSetVar AllowAlternateAuth 
+
+Set to 'yes' to specify whether or not you would like to use a secondary 
+authentication handler in conjunction with Apache::AuthExpire.  
+
+=head2 PerlSetVar TimeoutDebug <0 || 1>
+
+Set to 1, if you would like the module to write debugging information to the 
+error_log.
+
+=head2 PerlSetVar TimeLimit <seconds>
+
+Explicitly set the timeout limit to a value other than the 'DefaultLimit'.  This value must
+be less than the 'DefaultLimit', or else the module will revert to using the value of the
+'DefaultLimit' instead. Defaults to value of the 'DefaultLimit'.
+
+=head2 PerlSetVar TimeoutPurge <minutes>
+
+This removes the time file if 'TimeoutPurge' has expired, and if the 'stale' time file still
+exists.
+
+=head1 INSTALLATION NOTES
+
+This module uses the timestamps of files in the <SERVERROOT>/logs/authexpire directory
+to monitor sessions.  Therefore, you need to manually create a 'authexpire' directory,
+and your web server user (ie nobody), needs to have read/write access to this 
+directory.
 
 =head2 Caveats
 
   Does not work well with all browsers at this stage, please see
   mod_perl guide for more information.
+
   
 =head1 EXPORT
 
@@ -160,7 +275,8 @@ None by default.
 
 =head1 AUTHOR
 
-J. J. Horner jjhorner@bellsouth.net and Shannon Eric Peevey <speeves@unt.edu>
+J. J. Horner jjhorner@bellsouth.net 
+Ported by Shannon Eric Peevey <speeves@erikin.com>
 
 =head1 SEE ALSO
 
@@ -175,3 +291,5 @@ Can be found on CPAN.
 plaid and merlyn from http://perlmonks.org/ for general help and debugging.
 
 =cut
+
+
